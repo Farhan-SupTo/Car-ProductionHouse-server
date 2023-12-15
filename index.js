a97e10abff6d792ec967099ff8ce75169aa2ef8a
Your Name~ <you@example.com>
2023-12-16 01:24:21 +0600

    create a server for car productionhouse client using jwt to secure authorization,create delete update feature on data to show them in interface

---
+++ index.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+require('dotenv').config()
+const app =express()
+const port =process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2veuzlp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const verifyJWT =(req,res,next) =>{
+  console.log('hitting verified')
+  console.log(req.headers.authorization)
+  const authorization =req.headers.authorization
+  if(!authorization){
+    return res.status(401).send({error: true, message:'unauthorized access'})
+  }
+  const token =authorization.split(' ')[1]
+  console.log('token inside verify',token)
+  jwt.verify(token,process.env.Access_Token, (error,decoded)=>{
+   if(error){
+    return res.status(403).send({error: true, message:'unauthorized access'})
+   }
+   req.decoded =decoded
+   next()
+  })
+}
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const servicesCollection =client.db("CarProductionHouse").collection("Services")
+    const bookingCollection =client.db("CarProductionHouse").collection("booking")
+
+    // jwt routes
+
+    app.post('/jwt',(req,res)=>{
+      const user =req.body
+      console.log(user)
+      const token =jwt.sign(user,process.env.Access_Token, { expiresIn: '1h' })
+      console.log(token)
+      res.send({token})
+    })
+
+
+    // services routes
+app.get('/services',async(req,res)=>{
+    const cursor = servicesCollection.find();
+    const result = await cursor.toArray()
+    res.send(result)
+
+})
+
+app.get('/services/:id',async(req,res)=>{
+const id = req.params.id
+const query = {_id: new ObjectId(id)}
+
+const options = {
+    // Include only the `title` and `imdb` fields in the returned document
+    projection: { img: 1, title: 1, price:1 },
+  };
+
+const result = await servicesCollection.findOne(query,options);
+res.send(result)
+})
+
+// bookings
+
+app.get('/bookings', verifyJWT, async(req,res)=>{
+  const decoded = req.decoded
+  // come after verify
+    console.log('come after verify',decoded)
+
+if(decoded.email !== req.query.email){
+  return res.status(401).send({error: true, message:'forbidden access'})
+}
+
+    let query = {};
+    if(req.query?.email){
+        query = {email: req.query.email}
+    }
+    const result = await bookingCollection.find(query).toArray()
+   res.send(result)
+})
+
+app.post('/bookings',async(req,res)=>{
+    const booking =req.body
+    console.log(booking)
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result)
+})
+
+app.delete('/bookings/:id',async(req,res)=>{
+  const id =req.params.id
+  const query = { _id: new ObjectId(id)}
+  const result = await bookingCollection.deleteOne(query);
+  res.send(result)
+})
+
+app.patch('/bookings/:id',async(req,res)=>{
+  const id =req.params.id
+  const updatedBooking =req.body
+  console.log(updatedBooking)
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      status: updatedBooking.status
+    },
+  }
+  const result = await bookingCollection.updateOne(filter, updateDoc);
+  res.send(result)
+
+  
+})
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Car-Production-house is running')
+})
+
+app.listen(port,()=>{
+    console.log(`Car-production-house is running on port ${port}`)
+})
